4764d21ee54afd5b97895d028ec6bb9f9d9f394d
Vikas William <vikasdenobili@outlook.com>
2020-12-02 22:23:37 +0000

    Entityframwork addition using nuget Package

---
+++ RegistrationAPI/RegistrationAPI/Controllers/RegistrationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RegistrationService;
+
+
+namespace RegistrationAPI.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        public IEnumerable<MemberDetail2> Get()
+        {
+            using (RegistrationEntities entities = new RegistrationEntities())
+            {
+                return entities.MemberDetail2.ToList();
+            }
+        }
+    }
+}
